a20947b273d5be43c22fb1732ea269b44f6b98c7
Deeri1 <82732423+Deeri1@users.noreply.github.com>
2025-04-22 02:14:10 -0400

    dumbas

--- reanimhub.lua
+++ reanimhub.lua
@@ -202,18 +202,18 @@ function pdeathbaseplategame(nh)
             end
         elseif v:IsA("Accessory") then
             for i2,v2 in pairs(dummy:GetChildren()) do
-                if v2:IsA("Accessory") and v2.Name ~= "used" then -- a bunch of checks lol
+                if v2:IsA("Accessory") and v2:GetAttribute("used") then -- a bunch of checks lol
                     if v.Handle.Size == v2.Handle.Size then
                         if (v.Handle:findFirstChild("Mesh") and v2.Handle:findFirstChild("Mesh")) or (v.Handle:findFirstChild("SpecialMesh") and v2.Handle:findFirstChild("SpecialMesh")) then
                             if v.Handle:findFirstChild("Mesh") then
                                 if v.Handle.Mesh.MeshId == v2.Handle.Mesh.MeshId and v.Handle.Mesh.TextureId == v2.Handle.Mesh.TextureId then
                                     move(v.Handle,v2.Handle)
-                                    v2.Name = "used"
+                                    v2:SetAttribute("used", true)
                                 end
                             else
                                 if v.Handle.SpecialMesh.MeshId == v2.Handle.SpecialMesh.MeshId and v.Handle.SpecialMesh.TextureId == v2.Handle.SpecialMesh.TextureId then
                                     move(v.Handle,v2.Handle)
-                                    v2.Name = "used"
+                                    v2:SetAttribute("used", true)
                                 end
                             end
                         end
