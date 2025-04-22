d0a38ae26ed731a36f3748a55f0bec41b22bbcc7
Deeri1 <82732423+Deeri1@users.noreply.github.com>
2025-04-22 19:59:32 -0400

    e

--- adminV2.lua
+++ adminV2.lua
@@ -225,9 +225,10 @@ function sflinge(target)
 		end
         coroutine.wrap(nolcipf)()
 		wait(.1)
-		hrp.AssemblyLinearVelocity = Vector3.new(0, 1, 0)
+		hrp.AssemblyLinearVelocity = Vector3.new(0, 2, 0)
         hrp.RotVelocity = Vector3.new(0, 0, 0)
 		game:GetService'RunService'.Heartbeat:Wait()
+		game:GetService'RunService'.RenderStepped:Wait()
 		hrp.CFrame = CFrame.new(oldcf.X,oldcf.Y+3,oldcf.Z)
     end
 end
