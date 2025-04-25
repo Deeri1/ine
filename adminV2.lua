922e835c9e38645ce38c1faa1a692315f79eb6d9
Deeri1 <82732423+Deeri1@users.noreply.github.com>
2025-04-24 21:21:19 -0400

    a

--- adminV2.lua
+++ adminV2.lua
@@ -1269,11 +1269,11 @@ function test(traget)
 		tool.Parent = game.Players.LocalPlayer.Backpack
 		tool.Parent = game.Players.LocalPlayer.Character.Humanoid
 		tool.Parent = game.Players.LocalPlayer.Character
-        while wait() do
+        game:GetService("RunService").Heartbeat:Connect(function()
             tool.Handle.CFrame = game.Players.LocalPlayer.Character["Left Arm"].CFrame * CFrame.new(0, 0, 0) * CFrame.Angles(math.rad(60), math.rad(0), math.rad(60))
             tool.Handle.Velocity = Vector3.new(0, 100, 0)
             if nomore then
-                break
+                Disconnect()
             end
         end
 		
