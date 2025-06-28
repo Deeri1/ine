8db5cd93ae5d91c9f2531320e1447c929973299a
Deeri1 <82732423+Deeri1@users.noreply.github.com>
2025-06-28 15:53:27 -0400

    wfawewq21

--- reanimhub.lua
+++ reanimhub.lua
@@ -31,282 +31,299 @@ game.StarterGui:SetCore("SendNotification", {
 })
 
 function pdeathbaseplategame(nh)
-    --vars
-    local Players = game:FindFirstChildOfClass("Players")
-    local RunService = game:FindFirstChildOfClass("RunService")
-    --plr vars
-    local Player = Players.LocalPlayer
+   --vars
+	local Players = game:FindFirstChildOfClass("Players")
+	local RunService = game:FindFirstChildOfClass("RunService")
+	--plr vars
+	local Player = Players.LocalPlayer
 	local plrname = Player.Name
-    local Character = Player["Character"] or Player.CharacterAdded:Wait()
-    local char = Character
-    local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
-    local RootPart = Character:WaitForChild("HumanoidRootPart")
-    local Head = Character:WaitForChild("Head")
-    local RightArm = Character:WaitForChild("Right Arm")
-    local LeftArm = Character:WaitForChild("Left Arm")
-    local RightLeg = Character:WaitForChild("Right Leg")
-    local LeftLeg = Character:WaitForChild("Left Leg")
-    local Torso = Character:WaitForChild("Torso")
-    local Camera = workspace.CurrentCamera
-    local Mouse = Player:GetMouse()
-    workspace.FallenPartsDestroyHeight = 0/0
+	local Character = Player["Character"] or Player.CharacterAdded:Wait()
+	local char = Character
+	local Humanoid = Character:FindFirstChildWhichIsA("Humanoid")
+	--Humanoid:ChangeState("Physics")
+	--Humanoid.PlatformStand = true
+	local RootPart = Character:WaitForChild("HumanoidRootPart")
+	local Head = Character:WaitForChild("Head")
+	local RightArm = Character:WaitForChild("Right Arm")
+	local LeftArm = Character:WaitForChild("Left Arm")
+	local RightLeg = Character:WaitForChild("Right Leg")
+	local LeftLeg = Character:WaitForChild("Left Leg")
+	local Torso = Character:WaitForChild("Torso")
+	local Camera = workspace.CurrentCamera
+	local Mouse = Player:GetMouse()
+	workspace.FallenPartsDestroyHeight = 0/0
 
-    --functions
-    function cfmove(p0,p1)
-        p0.Cframe = p1.CFrame
-    end
+	--functions
+	function cfmove(p0,p1)
+		p0.Cframe = p1.CFrame
+	end
 
-    function move(p0,p1) --thanks Gelatek san
-        local AlignPosition = Instance.new("AlignPosition"); do
-            AlignPosition.MaxForce = 66666666666
-            AlignPosition.RigidityEnabled = true
-            AlignPosition.Responsiveness = 200
-            AlignPosition.Name = "AlignPosition1"
-            AlignPosition.Parent = p0
-        end
+	function move(p0,p1) --thanks Gelatek san
+		local AlignPosition = Instance.new("AlignPosition"); do
+			AlignPosition.MaxForce = 66666666666
+			AlignPosition.RigidityEnabled = true
+			AlignPosition.Responsiveness = 200
+			AlignPosition.Name = "AlignPosition1"
+			AlignPosition.Parent = p0
+		end
 
-        local AlignOrientation = Instance.new("AlignOrientation"); do
-            AlignOrientation.MaxTorque = 9e9
-            AlignOrientation.Responsiveness = 200
-            AlignOrientation.Name = "AlignOrientation"
-            AlignOrientation.Parent = p0
-        end
+		local AlignOrientation = Instance.new("AlignOrientation"); do
+			AlignOrientation.MaxTorque = 9e9
+			AlignOrientation.Responsiveness = 200
+			AlignOrientation.Name = "AlignOrientation"
+			AlignOrientation.Parent = p0
+		end
 
-        local Attachment1 = Instance.new("Attachment"); do
-            Attachment1.Position = Vector3.new(0,0,0)
-            Attachment1.Orientation = Vector3.new(0,0,0)
-            Attachment1.Name = "Attachment1"
-            Attachment1.Parent = p0
-        end
+		local Attachment1 = Instance.new("Attachment"); do
+			Attachment1.Position = Vector3.new(0,0,0)
+			Attachment1.Orientation = Vector3.new(0,0,0)
+			Attachment1.Name = "Attachment1"
+			Attachment1.Parent = p0
+		end
 
-        local Attachment2 = Instance.new("Attachment"); do
-            Attachment2.Parent = p1
-        end
+		local Attachment2 = Instance.new("Attachment"); do
+			Attachment2.Parent = p1
+		end
 
-        AlignPosition.Attachment0 = Attachment1
-        AlignPosition.Attachment1 = Attachment2
-        AlignOrientation.Attachment0 = Attachment1
-        AlignOrientation.Attachment1 = Attachment2
-    end
-    
-    --hat stuff
-    --finds the hats your missing and sets them as a hat in tempart
+		AlignPosition.Attachment0 = Attachment1
+		AlignPosition.Attachment1 = Attachment2
+		AlignOrientation.Attachment0 = Attachment1
+		AlignOrientation.Attachment1 = Attachment2
+	end
 
-    totalmh = 0
-    hataray = {}
-    idlist = {} -- holds the id of the hats that are needed 
-    function fmissinghats(nh)
-        for i,v in pairs(nh) do
-            for i,h in pairs(workspace.tempart:GetDescendants()) do
-                if h:FindFirstChild("Mesh") then
-                    if h.Mesh.TextureId == v then
-                        totalmh = totalmh + 1
-                        table.insert(idlist,h.Parent:GetAttribute("id"))
-                        table.insert(hataray,h)
-                    end
-                elseif h:FindFirstChild("SpecialMesh") then
-                    if h.SpecialMesh.TextureId == v then
-                        totalmh = totalmh + 1
-                        print(h.Parent:GetAttribute("id"))
-                        table.insert(idlist,h.Parent:GetAttribute("id"))
-                        table.insert(hataray,h)
-                    end
-                end
-            end
-        end
-    end
+	--hat stuff
+	--finds the hats your missing and sets them as a hat in tempart
+
+	totalmh = 0
+	hataray = {}
+	idlist = {} -- holds the id of the hats that are needed 
+	function fmissinghats(nh)
+		for i,v in pairs(nh) do
+			for i,h in pairs(workspace.tempart:GetDescendants()) do
+				if h:FindFirstChild("Mesh") then
+					if h.Mesh.TextureId == v then
+						totalmh = totalmh + 1
+						table.insert(idlist,h.Parent:GetAttribute("id"))
+						table.insert(hataray,h)
+					end
+				elseif h:FindFirstChild("SpecialMesh") then
+					if h.SpecialMesh.TextureId == v then
+						totalmh = totalmh + 1
+						print(h.Parent:GetAttribute("id"))
+						table.insert(idlist,h.Parent:GetAttribute("id"))
+						table.insert(hataray,h)
+					end
+				end
+			end
+		end
+	end
 
 
-    ----------------------------------------------------------------
-    --putting on missing hats :)
-    --testing if can -gh command :)
-    function testgh(ha)
-        local strangofhats = "-gh "
-        for i,v in ha do
-            strangofhats = strangofhats..v..", "
-        end
-        string.sub(strangofhats,1,string.len(strangofhats)-1)
-        --	print("here")
-        print(strangofhats)
+	----------------------------------------------------------------
+	--putting on missing hats :)
+	--testing if can -gh command :)
+	function testgh(ha)
+	print("fired")
+		local strangofhats = "-gh "
+		for i,v in ha do
+			strangofhats = strangofhats..v..", "
+		end
+		string.sub(strangofhats,1,string.len(strangofhats)-1)
+		--	print("here")
+		print(strangofhats)
+		local chatEvent = Instance.new("BindableEvent")
+		game.StarterGui:SetCore("CoreGuiChatConnections", {ChatWindow = {MessagePosted = chatEvent}})
+		-- Line above may error. Make sure to use pcall when using it and retry
+
+		chatEvent:Fire(strangofhats)
+		chatEvent:Destroy()
         local chatEvent = Instance.new("BindableEvent")
-        game.StarterGui:SetCore("CoreGuiChatConnections", {ChatWindow = {MessagePosted = chatEvent}})
-        -- Line above may error. Make sure to use pcall when using it and retry
+		game.StarterGui:SetCore("CoreGuiChatConnections", {ChatWindow = {MessagePosted = chatEvent}})
+		-- Line above may error. Make sure to use pcall when using it and retry
 
-        chatEvent:Fire(strangofhats)
-        chatEvent:Fire("-net")
-        chatEvent:Destroy()
-    end
-    function putonmhats(ha)
-        for i,v in pairs(ha) do
-            tmph = v.Parent:Clone()
-            --print("cloned"..v.Parent.Name.."")
-            tmph.Parent = workspace[plrname]
-        end
-        task.wait()
-        tempart:Destroy()
-    end
+		chatEvent:Fire("-net")
+		chatEvent:Destroy()
+	end
+	function putonmhats(ha)
+		for i,v in pairs(ha) do
+			tmph = v.Parent:Clone()
+			--print("cloned"..v.Parent.Name.."")
+			tmph.Parent = workspace[plrname]
+		end
+		task.wait()
+		tempart:Destroy()
+	end
 
-    --running the functions now
+	--running the functions now
 
-    fmissinghats(nh)
-    testgh(idlist)
-    wait(.5)
-    fmissinghats(nh)
-   -- putonmhats(hataray)
-    wait()
+	fmissinghats(nh)
+	testgh(idlist)
+	wait(.5)
+	fmissinghats(nh)
+	--putonmhats(hataray)
+	wait()
 
-    --dummy stuff
-    --dummy clone
-    for i, v in pairs(workspace:GetDescendants()) do
-        if v.Name == "Dummylolxdnoo" then
-            v:Destroy()
-        end
-    end
-    char = game.Players.LocalPlayer.Character
-    char.Archivable = true
-    for i, v in char:GetDescendants() do
-        v.Archivable = true
-        if v:IsA("BasePart") then
-            v.CanCollide = false
-        end
-    end
+	--dummy stuff
+	--dummy clone
+	for i, v in pairs(workspace:GetDescendants()) do
+		if v.Name == "Dummylolxdnoo" then
+			v:Destroy()
+		end
+	end
+	char = game.Players.LocalPlayer.Character
+	char.Archivable = true
+	for i, v in char:GetDescendants() do
+		v.Archivable = true
+		if v:IsA("BasePart") then
+			v.CanCollide = false
+		end
+	end
 
-    dummy = char:Clone()
-    wait(1) -- :( makes sure hats load
-    dummy.Name = "Dummylolxdnoo"
-    dummy.Parent = workspace
+	dummy = char:Clone()
+	wait(1) -- :( makes sure hats load
+	dummy.Name = "Dummylolxdnoo"
+	dummy.Parent = workspace
 
-    dummy.HumanoidRootPart.Position = char.HumanoidRootPart.Position
+	dummy.HumanoidRootPart.Position = char.HumanoidRootPart.Position
 
-    game.Players.LocalPlayer.ReplicationFocus = workspace[Player.Name]
-    for i, v in pairs(dummy:GetDescendants()) do
-        v.Archivable = true
-        if v:IsA("BasePart") then
-            v.Transparency = 1
-            v.CanCollide = false
-        end
-    end
+	game.Players.LocalPlayer.ReplicationFocus = workspace[Player.Name]
+	for i, v in pairs(dummy:GetDescendants()) do
+		v.Archivable = true
+		if v:IsA("BasePart") then
+			v.Transparency = .75
+			v.CanCollide = false
+		end
+	end
 
-    ---reanim main
-    Player.ReplicationFocus = workspace
+	---reanim main
+	Player.ReplicationFocus = workspace
 	settings()["Physics"].PhysicsEnvironmentalThrottle = Enum.EnviromentalPhysicsThrottle.Disabled
 	settings()["Physics"].AllowSleep = false
 	settings()["Physics"].ForceCSGv2 = false
 	settings()["Physics"].DisableCSGv2 = true
 	settings()["Physics"].UseCSGv2 = false
-    task.spawn(function()
-        dummy.Humanoid.BreakJointsOnDeath = false
-        game:GetService("StarterGui"):SetCore("ResetButtonCallback", false) -- kills player
-        task.wait(Players.RespawnTime + game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValue() / 750)
-        local Head = Character:FindFirstChild("Head")
-        Head:BreakJoints() 
-        print("dead")
-        game:GetService("StarterGui"):SetCore("ResetButtonCallback", true)
-    end)
-    for i,v in pairs(char:GetChildren()) do -- making sure hats line up
-        if v:IsA("BasePart") and v.Name ~= "HumanoidRootPart" then
-            if dummy:FindFirstChild(v.Name) then
-                move(v,dummy[v.Name])
-            end
-        elseif v:IsA("Accessory") then
-            for i2,v2 in pairs(dummy:GetChildren()) do
-                if v2:IsA("Accessory") and not v2:GetAttribute("used") then -- a bunch of checks lol
-                    if v.Handle.Size == v2.Handle.Size then
-                        if (v.Handle:findFirstChild("Mesh") and v2.Handle:findFirstChild("Mesh")) or (v.Handle:findFirstChild("SpecialMesh") and v2.Handle:findFirstChild("SpecialMesh")) then
-                            if v.Handle:findFirstChild("Mesh") then
-                                if v.Handle.Mesh.MeshId == v2.Handle.Mesh.MeshId and v.Handle.Mesh.TextureId == v2.Handle.Mesh.TextureId then
-                                    move(v.Handle,v2.Handle)
-                                    v2:SetAttribute("used", true)
-                                end
-                            else
-                                if v.Handle.SpecialMesh.MeshId == v2.Handle.SpecialMesh.MeshId and v.Handle.SpecialMesh.TextureId == v2.Handle.SpecialMesh.TextureId then
-                                    move(v.Handle,v2.Handle)
-                                    v2:SetAttribute("used", true)
-                                end
-                            end
-                        end
-                    end
-                end
-            end
-        end
-    end
-    --char.Parent = dummy
+	task.spawn(function()
+		dummy.Humanoid.BreakJointsOnDeath = false
+	game:GetService("StarterGui"):SetCore("ResetButtonCallback", false) -- kills player
+	task.wait(Players.RespawnTime + game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValue() / 750)
+	wait(1)
+	local Head = char:FindFirstChild("Head")
+	Head:BreakJoints() 
+	print("dead")
+	game:GetService("StarterGui"):SetCore("ResetButtonCallback", true)
 
-    --velocity loop
-    task.spawn(function()
-        RunService.PreSimulation:Connect(function()
-            if not dummy:FindFirstChild("HumanoidRootPart") then
-                return
-            end
-            Velocity = Vector3.new(dummy["HumanoidRootPart"].CFrame.LookVector.X * 85, dummy["Head"].Velocity.Y * 4, dummy["HumanoidRootPart"].CFrame.LookVector.Z * 85)
-            for i,v in char:GetDescendants() do
-                if v:IsA("BasePart") then
-                v.CanCollide = false
-                    if v and v.Parent then
-                        v.AssemblyLinearVelocity = Velocity
-                    end
-                end
-            end
-        end)
-    end)
-    --camera stuff
-    local CurCameraOffset = workspace.CurrentCamera.CFrame
-    workspace.CurrentCamera.CFrame = CurCameraOffset
-    Player.Character = dummy
-    workspace.CurrentCamera.CFrame = CurCameraOffset
-    workspace.CurrentCamera.CameraSubject = dummy.Humanoid
-    workspace.CurrentCamera.CFrame = CurCameraOffset
+	end)
+	
+	for i,v in pairs(char:GetChildren()) do -- making sure hats line up
+		if v:IsA("BasePart") and v.Name ~= "HumanoidRootPart" then
+			if dummy:FindFirstChild(v.Name) then
+				move(v,dummy[v.Name])
+			end
+		elseif v:IsA("Accessory") then
+			for i2,v2 in pairs(dummy:GetChildren()) do
+				if v2:IsA("Accessory") and not v2:GetAttribute("used") then -- a bunch of checks lol
+					if v.Handle.Size == v2.Handle.Size then
+						if (v.Handle:findFirstChild("Mesh") and v2.Handle:findFirstChild("Mesh")) or (v.Handle:findFirstChild("SpecialMesh") and v2.Handle:findFirstChild("SpecialMesh")) then
+							if v.Handle:findFirstChild("Mesh") then
+								if v.Handle.Mesh.MeshId == v2.Handle.Mesh.MeshId and v.Handle.Mesh.TextureId == v2.Handle.Mesh.TextureId then
+									move(v.Handle,v2.Handle)
+									v2:SetAttribute("used", true)
+								end
+							else
+								if v.Handle.SpecialMesh.MeshId == v2.Handle.SpecialMesh.MeshId and v.Handle.SpecialMesh.TextureId == v2.Handle.SpecialMesh.TextureId then
+									move(v.Handle,v2.Handle)
+									v2:SetAttribute("used", true)
+								end
+							end
+						end
+					end
+				end
+			end
+		end
+	end
+	--char.Parent = dummy
 
-    --fling stuff
-    p0 = char.HumanoidRootPart
-    p1 = dummy["Right Arm"]
-    local AlignPosition = Instance.new("AlignPosition"); do
-        AlignPosition.MaxForce = 66666666666
-        AlignPosition.RigidityEnabled = true
-        AlignPosition.Responsiveness = 200
-        AlignPosition.Name = "AlignPosition1"
-        AlignPosition.Parent = p0
-    end
-    local Attachment1 = Instance.new("Attachment"); do
-        Attachment1.Position = Vector3.new(0,0,0)
-        Attachment1.Orientation = Vector3.new(0,0,0)
-        Attachment1.Name = "Attachment1"
-        Attachment1.Parent = p0
-    end
+	--velocity loop
+	task.spawn(function()
+	RunService.PreSimulation:Connect(function()
+		if not dummy:FindFirstChild("HumanoidRootPart") then
+			return
+		end
+		Velocity = Vector3.new(dummy["HumanoidRootPart"].CFrame.LookVector.X * 85, dummy["Head"].Velocity.Y * 4, dummy["HumanoidRootPart"].CFrame.LookVector.Z * 85)
+		for i,v in char:GetChildren() do
+			if v:IsA("BasePart") then
+				v.CanCollide = false
+				if v and v.Parent then
+					v.Velocity = Velocity
+				end
+			end
+		end
+	end)
+	while wait() do
+		for i,v in game.workspace[plrname]:GetChildren() do
+			if v:IsA("Accessory") then
+				v.Handle.Velocity = Vector3.new(300, 300, 300)
+			end
+		end
+	end
+	end)
+	--camera stuff
+	local CurCameraOffset = workspace.CurrentCamera.CFrame
+	workspace.CurrentCamera.CFrame = CurCameraOffset
+	Player.Character = dummy
+	workspace.CurrentCamera.CFrame = CurCameraOffset
+	workspace.CurrentCamera.CameraSubject = dummy.Humanoid
+	workspace.CurrentCamera.CFrame = CurCameraOffset
 
-    local flingatch = Instance.new("Attachment"); do
-        flingatch.Parent = p1
-    end
+	--fling stuff
+	p0 = char.HumanoidRootPart
+	p1 = dummy["Right Arm"]
+	local AlignPosition = Instance.new("AlignPosition"); do
+		AlignPosition.MaxForce = 66666666666
+		AlignPosition.RigidityEnabled = true
+		AlignPosition.Responsiveness = 200
+		AlignPosition.Name = "AlignPosition1"
+		AlignPosition.Parent = p0
+	end
+	local Attachment1 = Instance.new("Attachment"); do
+		Attachment1.Position = Vector3.new(0,0,0)
+		Attachment1.Orientation = Vector3.new(0,0,0)
+		Attachment1.Name = "Attachment1"
+		Attachment1.Parent = p0
+	end
 
-    AlignPosition.Attachment0 = Attachment1
-    AlignPosition.Attachment1 = flingatch
-    local ABV = Instance.new("BodyAngularVelocity")
-    ABV.P = 1/0
-    ABV.AngularVelocity = Vector3.new(0,0,0)
-    ABV.Name = "AngularVelocity"
-    ABV.Parent = char.HumanoidRootPart
-    char.HumanoidRootPart.Transparency = .5
-    char.HumanoidRootPart.AngularVelocity.AngularVelocity = Vector3.new(math.huge,math.huge,math.huge)
+	local flingatch = Instance.new("Attachment"); do
+		flingatch.Parent = p1
+	end
 
-    _G.fling = function(instance,duration)
-        if duration == nil then
-            duration = 3
-        end
-        local flingatch2 = Instance.new("Attachment"); do
-            flingatch2.Parent = instance
-        end
-        AlignPosition.Attachment1 = flingatch2
-        char.HumanoidRootPart.AngularVelocity.AngularVelocity = Vector3.new(math.huge,math.huge,math.huge)
-        wait(duration)
-        flingatch2:Destroy()
-        AlignPosition.Attachment1 = flingatch
-    end
+	AlignPosition.Attachment0 = Attachment1
+	AlignPosition.Attachment1 = flingatch
+	local ABV = Instance.new("BodyAngularVelocity")
+	ABV.P = 1/0
+	ABV.AngularVelocity = Vector3.new(0,0,0)
+	ABV.Name = "AngularVelocity"
+	ABV.Parent = char.HumanoidRootPart
+	char.HumanoidRootPart.Transparency = .5
+	char.HumanoidRootPart.AngularVelocity.AngularVelocity = Vector3.new(math.huge,math.huge,math.huge)
 
-    --runs the script
-    wait(1)
-    scripthere = _G.scripthere
-    coroutine.wrap(scripthere)()
+	_G.fling = function(instance,duration)
+		if duration == nil then
+			duration = 3
+		end
+		local flingatch2 = Instance.new("Attachment"); do
+			flingatch2.Parent = instance
+		end
+		AlignPosition.Attachment1 = flingatch2
+		char.HumanoidRootPart.AngularVelocity.AngularVelocity = Vector3.new(math.huge,math.huge,math.huge)
+		wait(duration)
+		flingatch2:Destroy()
+		AlignPosition.Attachment1 = flingatch
+	end
 
+	--runs the script
+	scripthere = _G.scripthere
+	--wait(6)
+	coroutine.wrap(scripthere)()
 end--end of baseplate reanim
 
 function botbasic(nh)
